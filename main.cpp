04488672247795a076c00a4b30f25559cada22a6
ricardj <43671359+ricardj@users.noreply.github.com>
2019-03-12 09:44:09 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Parser.h"
+#include "Delaunay.h"
+
+void main(int argc, const char* argv[])
+{
+	Mesh mesh;
+	std::cout << "Parsing input file. Please wait...\n";
+
+	if (!parsePoints("input.txt", mesh.vertices))
+	{
+		printf("ERROR! Could not parse input file\n");
+		return;
+	}
+
+	std::cout << " [Done]\n" << std::endl;
+	std::cout << "Creating mesh. Please wait...\n";
+
+	triangulateMesh(mesh);
+
+	std::cout << " [Done]\n" << std::endl;
+	std::cout << "Saving PLY file. Please wait...\n";
+
+	if (!saveMeshToPLY(mesh, "ouput.ply"))
+	{
+		printf("ERROR! Could not save mesh to PLY file\n");
+		return;
+	}
+
+	std::cout << " [Done]\n" << std::endl;
+	//getchar();
+}
+
